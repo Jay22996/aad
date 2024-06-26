64ab1b960e4c79c289905bcf89614ca2fabb49cc
Jay22996 <jay00289@gmail.com>
2024-06-26 18:54:11 +0530

    sdfd

--- controller/cotroller.js
+++ controller/cotroller.js
@@ -54,7 +54,7 @@ exports.deletem = async (req, res) => {
 };
 
 exports.show = async (req, res) => {
-    var data = await mark.find().populate("student_id")
+    var data = await post.find().populate("student_id")
   
     res.status(200).json({
       status: "find",
