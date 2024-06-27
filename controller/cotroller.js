c477b3470f466275ea06ac97aa6dcf331593e4ea
Jay22996 <jay00289@gmail.com>
2024-06-27 08:12:34 +0530

    all done

--- controller/cotroller.js
+++ controller/cotroller.js
@@ -2,30 +2,25 @@ var post = require("../model/studentdata");
 var mark = require("../model/mark");
 
 exports.addstudent = async (req, res) => {
-
-  var chaq = await post.findOne({name:req.body.name})
+  var chaq = await post.find({ name: req.body.name });
   console.log(req.body.name);
-  if(chaq === null){
-    var data = await post.create(req.body);
-    var id = data._id;
-  
-    req.body.student_id = id;
-    await mark.create(req.body);
-  
-    res.status(200).json({
-      status: "add done",
-      data,
-    });
-  }else{
-
+  if (chaq.length != 0) {
     res.status(404).json({
       status: "user already in use",
       chaq,
     });
+  } else {
+    var data = await post.create(req.body);
+    var id = data._id;
+
+    req.body.student_id = id;
+    await mark.create(req.body);
+
+    res.status(200).json({
+      status: "add done",
+      data,
+    });
   }
-
-
- 
 };
 
 exports.updatemark = async (req, res) => {
@@ -68,10 +63,10 @@ exports.deletem = async (req, res) => {
 };
 
 exports.show = async (req, res) => {
-    var data = await post.find()
-  
-    res.status(200).json({
-      status: "find",
-      data,
-    });
-  };
+  var data = await post.find();
+
+  res.status(200).json({
+    status: "find",
+    data,
+  });
+};
